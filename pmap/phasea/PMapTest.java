42323bef3b150e42b824725b316b8c0b41ff2ac9
kevinp0122 <61161879+kevinp0122@users.noreply.github.com>
2022-02-23 12:07:18 -0600

    Update PMapTest.java

--- pmap/phasea/PMapTest.java
+++ pmap/phasea/PMapTest.java
@@ -1,6 +1,6 @@
 /*
- * Phase A <studentA EID><studentB EID>
- * Phase B <studentB EID><studentA EID>
+ * Phase A <hj6982><kwp535>
+ * Phase B <kwp535><hj6982>
  */
 
 package pmap.phasea;
@@ -30,4 +30,51 @@ public class PMapTest {
     }
 
     // TODO add more test cases to test all implemented methods
+    public void testSize()
+    {
+    	PMap m = new PMap();
+        m.put(1, 2);
+        m.put(2, 3);
+        assertEquals(2, m.size().intValue());
+    }
+    public void testIsEmpty()
+    {
+    	PMap m = new PMap();
+    	assertTrue(m.isEmpty());
+    }
+     public void testContainsKey()
+     {
+    	 PMap m = new PMap();
+    	 m.put(1,  2);
+    	 m.put(2,  3);
+    	 assertTrue(m.containsKey(2));
+     }
+     public void testRemove()
+     {
+    	 PMap m = new PMap();
+    	 m.put(1,  2);
+    	 m.put(2,  3);
+    	 m.remove(2);
+    	 assertTrue(!m.containsKey(2));
+     }
+     public void testPutAll()
+     {
+    	 Integer[] keys = {1, 2, 3};
+    	 Integer[] values = {2, 3, 4};
+    	 PMap m = new PMap();
+    	 m.putAll(keys,  values);
+    	 assertTrue(m.containsKey(1));
+    	 assertTrue(m.containsKey(2));
+    	 assertTrue(m.containsKey(3));
+    	 assertTrue(m.containsValue(2));
+    	 assertTrue(m.containsValue(3));
+    	 assertTrue(m.containsValue(4));
+
+     }
+     public void testClear()
+     {
+    	 PMap m = new PMap();
+    	 m.clear();
+    	 assertTrue(m.isEmpty());
+     }
 }
